e6e26c4ba1d55f551e4598ee0e94e660915f3420
Armin Dhamrait <armindhamrait@gmail.com>
2023-11-30 17:33:21 -0500

    Fully Updated Unit Tests

--- FourPointsUnitTest/UnitTests.cpp
+++ FourPointsUnitTest/UnitTests.cpp
@@ -25,7 +25,7 @@ namespace UnitTests
 		{
 			struct POINTS TestPoint[4];			//create a testpoint that takes the two float values x and y. 
 
-			TestPoint[0] = { 0,0 };				
+			TestPoint[0] = { 0,0 };
 			TestPoint[1] = { 0,2 };
 			TestPoint[2] = { 3,2 };
 			TestPoint[3] = { 3,0 };
@@ -43,14 +43,28 @@ namespace UnitTests
 			TestPoint[1] = { 5,2 };
 			TestPoint[2] = { 7,6 };
 			TestPoint[3] = { 3,4 };
-			
+
+			bool result = IsRectangle(TestPoint);
+
+			Assert::IsFalse(result);
+		}
+
+		TEST_METHOD(TestPointsNotValidRectangle2)
+		{
+			struct POINTS TestPoint[4];
+
+			TestPoint[0] = { 1,2 };
+			TestPoint[1] = { 3,4 };
+			TestPoint[2] = { 5,6 };
+			TestPoint[3] = { 7,8 };
+
 			bool result = IsRectangle(TestPoint);
 
 			Assert::IsFalse(result);
 		}
 
 
-		TEST_METHOD(TestNegativePoints1)			//though negative values cannot be used during scanf portion of our main 
+		TEST_METHOD(TestNegativePoints)			//though negative values cannot be used during scanf portion of our main 
 		{										//we still need to test it to ensure IsRectangle is working as intended. 
 			struct POINTS TestPoint[4];
 
@@ -64,98 +78,92 @@ namespace UnitTests
 			Assert::IsFalse(result);
 		}
 
-		
 
-		TEST_METHOD(TestNegativePoints2)	{	
+		TEST_METHOD(TestSquare)
+		{
 			struct POINTS TestPoint[4];
 
-			TestPoint[0] = { 0, 1 };
-			TestPoint[1] = { 3,-2 };
-			TestPoint[2] = { -3,3 };
-			TestPoint[3] = { 3,1 };
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 0, 1 };
+			TestPoint[2] = { 1, 1 };
+			TestPoint[3] = { 1, 0 };
+
+			bool result = IsRectangle(TestPoint);
+
+			Assert::IsTrue(result);
+		}
+
+
+		TEST_METHOD(TestPointsIfParallelogram)
+		{
+			struct POINTS TestPoint[4];
+
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 2, 2 };
+			TestPoint[2] = { 4, 2 };
+			TestPoint[3] = { 2, 0 };
+
+			bool result = IsRectangle(TestPoint);
+
+			Assert::IsTrue(result);
+		}
+
+		TEST_METHOD(TestPointsIfTrapezoid)
+		{
+			struct POINTS TestPoint[4];
+
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 1, 1 };
+			TestPoint[2] = { 3, 1 };
+			TestPoint[3] = { 2, 0 };
+
+			bool result = IsRectangle(TestPoint);
+
+			Assert::IsTrue(result);
+		}
+
+
+
+		TEST_METHOD(TestPointsInALine)
+		{
+			struct POINTS TestPoint[4];
+
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 1, 1 };
+			TestPoint[2] = { 2, 2 };
+			TestPoint[3] = { 3, 3 };
 
 			bool result = IsRectangle(TestPoint);
 
 			Assert::IsFalse(result);
 		}
 
-
-		TEST_METHOD(TestNegativePoints3) {
+		TEST_METHOD(TestRectangleWithEqualSides)
+		{
 			struct POINTS TestPoint[4];
 
-			TestPoint[0] = { 2, 1 };
-			TestPoint[1] = { 1,-2 };
-			TestPoint[2] = { -3,2 };
-			TestPoint[3] = { 3,-1 };
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 0, 3 };
+			TestPoint[2] = { 4, 3 };
+			TestPoint[3] = { 4, 0 };
 
 			bool result = IsRectangle(TestPoint);
 
-			Assert::IsFalse(result);
+			Assert::IsTrue(result);
 		}
 
-		TEST_METHOD(TestNegativePoints4) {
+		TEST_METHOD(TestRectangleIfThin)
+		{
 			struct POINTS TestPoint[4];
 
-			TestPoint[0] = { 1, 1 };
-			TestPoint[1] = { 3,-2 };
-			TestPoint[2] = { -4,3 };
-			TestPoint[3] = { 3,2 };
+			TestPoint[0] = { 0, 0 };
+			TestPoint[1] = { 0, 1 };
+			TestPoint[2] = { 10, 1 };
+			TestPoint[3] = { 10, 0 };
 
 			bool result = IsRectangle(TestPoint);
 
-			Assert::IsFalse(result);
-		}
-
-		TEST_METHOD(TestNegativePoints5) {
-			struct POINTS TestPoint[4];
-
-			TestPoint[0] = { 2, 1 };
-			TestPoint[1] = { 2,-2 };
-			TestPoint[2] = { -3,3 };
-			TestPoint[3] = { 3,3 };
-
-			bool result = IsRectangle(TestPoint);
-
-			Assert::IsFalse(result);
-		}
-
-		TEST_METHOD(TestNegativePoints6) {
-			struct POINTS TestPoint[4];
-
-			TestPoint[0] = { -3, 1 };
-			TestPoint[1] = { 3,-4 };
-			TestPoint[2] = { -3,-4 };
-			TestPoint[3] = { 3,2 };
-
-			bool result = IsRectangle(TestPoint);
-
-			Assert::IsFalse(result);
-		}
-
-		TEST_METHOD(TestNegativePoints7) {
-			struct POINTS TestPoint[4];
-
-			TestPoint[0] = { 5, 1 };
-			TestPoint[1] = { 3,-5 };
-			TestPoint[2] = { -2,3 };
-			TestPoint[3] = { 3,3 };
-
-			bool result = IsRectangle(TestPoint);
-
-			Assert::IsFalse(result);
-		}
-
-		TEST_METHOD(TestNegativePoints8) {
-			struct POINTS TestPoint[4];
-
-			TestPoint[0] = { 0, 4 };
-			TestPoint[1] = { 3,-1 };
-			TestPoint[2] = { -1,3 };
-			TestPoint[3] = { 3,5 };
-
-			bool result = IsRectangle(TestPoint);
-
-			Assert::IsFalse(result);
+			Assert::IsTrue(result);
 		}
 	};
 
